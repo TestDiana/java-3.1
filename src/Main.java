93c4155566c85c2600bd3a4cffba9669f59bab5d
Diana Vafina <Dianochka.25@mail.ru>
2020-12-23 21:24:20 +0300

    Main.java created

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 2000_00;
+        int miles = cost / 20 / 100;
+        System.out.println(miles);
+
+    }
+}
